f46b8f5286d57b570916e82f178b10da4c4df3dd
ndautukwabala-lab <ndautukwabala@gmail.com>
2025-12-07 18:27:53 +0200

    load.py

--- energy-etl-pipeline/load.py
+++ energy-etl-pipeline/load.py
@@ -1,13 +1,26 @@
 import pandas as pd
+import json
 from sqlalchemy import create_engine
 
 def load_data():
-    df = pd.read_csv("data/sample_clean.csv")
+    file_path = "/content/data/sample_raw.json"
 
+    # Load JSON
+    with open(file_path, "r") as f:
+        raw = json.load(f)
+
+    # Extract the actual table data
+    records = raw["response"]["data"]
+
+    # Convert to DataFrame
+    df = pd.DataFrame(records)
+
+    # Load into SQLite
     engine = create_engine("sqlite:///energy.db")
     df.to_sql("energy_data", engine, if_exists="replace", index=False)
 
-    print("Data loaded into SQLite database energy.db")
+    print("✅ Data loaded into SQLite database energy.db")
+    print("📌 Rows loaded:", len(df))
 
 if __name__ == "__main__":
     load_data()
